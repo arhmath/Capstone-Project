b30beca0a99d93a6ffee0dbd05159c7317b38b32
fahmifath <fahmifthur@gmail.com>
2026-05-13 23:19:09 +0700

    fix: streak logic

--- src/services/streak.service.js
+++ src/services/streak.service.js
@@ -1,61 +1,55 @@
-const { differenceInCalendarDays } = require('date-fns');
+const { differenceInCalendarDays, startOfDay } = require('date-fns');
 const prisma = require('../config/prisma');
 
 const updateUserStreak = async (userId, tx = prisma) => {
-  const today = new Date();
+    const today = startOfDay(new Date());
 
-  let streak = await tx.userStreak.findUnique({
-    where: { userId },
-  });
-
-  if (!streak) {
-    return tx.userStreak.create({
-      data: {
-        userId,
-        currentStreak: 1,
-        longestStreak: 1,
-        lastActivityDate: today,
-      },
+    let streak = await tx.userStreak.findUnique({
+        where: { userId },
     });
-  }
 
-  const diffDays = differenceInCalendarDays(
-    today,
-    streak.lastActivityDate
-  );
+    if (!streak) {
+        return tx.userStreak.create({
+            data: {
+                userId,
+                currentStreak: 1,
+                longestStreak: 1,
+                lastActivityDate: today,
+            },
+        });
+    }
 
-  // Sudah aktif hari ini
-  if (diffDays === 0) {
-    return streak;
-  }
+    const lastActivityDate = startOfDay(new Date(streak.lastActivityDate));
 
-  // Beruntun
-  if (diffDays === 1) {
-    const newCurrentStreak = streak.currentStreak + 1;
+    const diffDays = differenceInCalendarDays(
+        today,
+        lastActivityDate
+    );
+
+    // Sudah aktif hari ini
+    if (diffDays === 0) {
+        return streak;
+    }
+
+    let newCurrentStreak = 1; 
+
+    // Beruntun
+    if (diffDays === 1) {
+        newCurrentStreak = streak.currentStreak + 1;
+    }
+
+    const newLongestStreak = Math.max(streak.longestStreak, newCurrentStreak);
 
     return tx.userStreak.update({
-      where: { userId },
-      data: {
-        currentStreak: newCurrentStreak,
-        longestStreak: Math.max(
-          streak.longestStreak,
-          newCurrentStreak
-        ),
-        lastActivityDate: today,
-      },
+        where: { userId },
+        data: {
+            currentStreak: newCurrentStreak,
+            longestStreak: newLongestStreak,
+            lastActivityDate: today,
+        },
     });
-  }
-
-  // Putus → mulai lagi dari 1
-  return tx.userStreak.update({
-    where: { userId },
-    data: {
-      currentStreak: 1,
-      lastActivityDate: today,
-    },
-  });
 };
 
 module.exports = {
-  updateUserStreak,
+    updateUserStreak,
 };
